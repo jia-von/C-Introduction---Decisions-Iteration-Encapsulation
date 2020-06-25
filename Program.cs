1e395ab31a9dcc22f1afb0df0fbc98b1ca42d454
Jia <jiavon@ualberta.ca>
2020-06-25 17:19:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projects_25_Jun_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
