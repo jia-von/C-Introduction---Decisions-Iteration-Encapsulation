4d817c610298b0a6b36852c5ee1a91678df8cd66
Jia <jiavon@ualberta.ca>
2020-06-25 23:21:22 -0600

    Completed problem 1: power to 3 solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Projects_25_Jun_2020
     {
         static void Main(string[] args)
         {   
-            
+            //Assignment on 25-Jun-2020: Problem 1 
+            //Telling user to input value
+            Console.WriteLine("User please add integer: ");
+
+            //Convert user input value to integer
+            int firstNum = Int32.Parse( Console.ReadLine());
+
+            //Display answer that int ^3
+            //Math.Power method @https://stackoverflow.com/questions/3034604/is-there-an-exponent-operator-in-c
+            //Conver integer to string @https://stackoverflow.com/questions/26243602/cannot-implicity-convert-type-double-to-string
+            string result = Math.Pow(firstNum, 3).ToString();
+            Console.WriteLine(result);
+        
+        
         }
     }
 }
