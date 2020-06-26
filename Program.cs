87200bff49b49d1dd25de0496c803e0fc2a0d035
Jia <jiavon@ualberta.ca>
2020-06-25 18:24:11 -0600

    starting practice promblems blank

--- Program.cs
+++ Program.cs
@@ -5,15 +5,8 @@ namespace Projects_25_Jun_2020
     class Program
     {
         static void Main(string[] args)
-        {   //Greet the user
-            Console.WriteLine("Hello World!");
-
-            // Let the user know how to exit the program.
-
-            Console.WriteLine( "Press the enter key to close the program..." );
+        {   
             
-            //Pause input before continuing
-            Console.ReadLine();
         }
     }
 }
